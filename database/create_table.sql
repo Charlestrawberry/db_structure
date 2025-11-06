7472a8d663d2ccf578cd1da85cc16c9bed0cba37
Oduks <charlestrawb_odus@yahoo.com>
2025-11-06 08:07:30 +0100

    Inserted Data into the tables

--- database/create_table.sql
+++ database/create_table.sql
@@ -19,12 +19,28 @@ CREATE TABLE teachers (
 CREATE TABLE classes (
     class_id INT PRIMARY KEY AUTO_INCREMENT,
     class_name VARCHAR(20),
-    teacher_id INT FOREIGN KEY,
+    teacher_id INT,
+    FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id), 
 );
 
 CREATE TABLE scores (
     score_id INT PRIMARY KEY AUTO_INCREMENT,
-    student_id INT FOREIGN KEY,
-    class_id INT FOREIGN KEY,
-    score INT ,
-)
+    student_id INT,
+    class_id INT,
+    score INT,
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (class_id) REFERENCES classes(class_id)
+);
+
+-- Insert sample classes
+INSERT INTO classes (class_name, teacher_id) VALUES
+('JSS1', 1),
+('JSS2', 2),
+('JSS3', 3);
+
+INSERT INTO scores (student_id, class_id, score) VALUES
+(1, 1, 85), (2, 1, 78), (3, 1, 92),
+(4, 2, 88), (5, 2, 73), (6, 2, 95),
+(7, 3, 90), (8, 3, 64), (9, 3, 81),
+(10, 3, 99), (1, 2, 67), (3, 2, 80),
+(2, 3, 74), (4, 1, 88), (5, 1, 76);
---
+++ database/insert_tables.sql
@@ -0,0 +1,21 @@
+USE school_db;
+
+-- Insert sample students
+INSERT INTO students (name, age, gender) VALUES
+('Alice', 13, 'female')
+('Brian', 14, 'Male'),
+('Chika', 13, 'Female'),
+('David', 14, 'Male'),
+('Ella', 13, 'Female'),
+('Femi', 14, 'Male'),
+('Grace', 13, 'Female'),
+('Hassan', 15, 'Male'),
+('Ife', 14, 'Female'),
+('James', 15, 'Male');
+
+INSERT INTO teachers (name, subjects) VALUES
+('Mr. Ade', 'Mathematics'),
+('Mrs. Okoro', 'English'),
+('Mr. Musa', 'Science');
+
+INSERT INTO classes ()
